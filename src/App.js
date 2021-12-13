b7891f67bae70e5f10bbd1e42f11b829b14d313d
LucasUzoka <92759285+LucasUzoka@users.noreply.github.com>
2021-12-12 23:42:15 -0500

    new register page, form validation, success pop up transition not yet functional

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import HeadJumbo from "./components/HeadJumbo";
 import Products from "./components/Products";
 import {ProductData} from "./components/Products/data";
-// import Navbar from "./components/Navbar";
 import { GlobalStyle } from "./globalStyle";
 import Footer from "./components/Footer";
+// import Registration from "./pages/RegisterPage/Register";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Register from "./pages/RegisterPage/Register";
+import RegisterForm from "./pages/RegisterPage";
 
 
 function App() {
@@ -16,9 +20,11 @@ function App() {
     
     <Router>
       <GlobalStyle />
-      {/* <Navbar /> */}
+      
+      
       <HeadJumbo />
       <Products heading='Select Your Favorites' data={ProductData}/>
+      <RegisterForm />
       <Footer />
     </Router>
   );
